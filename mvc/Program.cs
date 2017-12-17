e311c37160ed2ddcb2a7125211f6fe7d7e67488c
van <vanfreeride@gmail.com>
2017-12-17 22:07:15 +0300

    deleted bins

--- mvc/Program.cs
+++ mvc/Program.cs
@@ -19,7 +19,7 @@ namespace mvc
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                //.UseStartup<Startup>()
+                .UseStartup<Startup>()
                 .Build();
     }
 }
